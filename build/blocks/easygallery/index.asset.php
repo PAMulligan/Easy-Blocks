e2105487bc391b73a2e0bcf381c64ea4367aa2a3
PaulDRev <paul@dentalrevenue.com>
2024-04-03 14:40:49 -0400

    Remove specific padding from Easy Curve

--- build/blocks/easygallery/index.asset.php
+++ build/blocks/easygallery/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '07b9d46c2e2dbca385a5');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'cdda60421c83e3aef1bb');
--- build/blocks/easygroup/index.asset.php
+++ build/blocks/easygroup/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks'), 'version' => 'bdd113d4e6bdaa316ce9');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks'), 'version' => 'f46e7418e36fb9ff17ec');
