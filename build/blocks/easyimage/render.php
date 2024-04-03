8b8b6af44ee4b4d52a177ade7e0e16b27a8aee85
PaulDRev <paul@dentalrevenue.com>
2024-04-03 12:46:20 -0400

    Hover gallery working on desktop in testing

--- build/blocks/easyimage/render.php
+++ build/blocks/easyimage/render.php
@@ -7,7 +7,7 @@ $image_uri = wp_get_attachment_image_url($attributes['imageId'] ?? 0);
 $image_uri_large = wp_get_attachment_image_url($attributes['imageId'] ?? 0, "large");
 ?>
 <div <? echo $block_wrapper_attributes; ?>>
-  <img data-large-size="<? echo $image_uri_large; ?>" src="<? echo $image_uri; ?>" class="thumb" />
+  <img data-large-size="<? echo $image_uri_large; ?>" src="<? echo $image_uri; ?>" width="180" height="180" class="thumb" />
   <? if ($attributes['hoverText']) : ?>    
     <div class="easy-gallery-overlay">
       <div class="easy-gallery-hover-text">
